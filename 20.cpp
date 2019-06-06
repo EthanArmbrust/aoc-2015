086a2580e5db7ec344d85a696e4a24f596f1595b
Ethan Armbrust <EthanArmbrust@github.com>
2019-06-06 11:38:46 -0400

    day 20 part 1

---
+++ 20.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int sum_presents(int n);
+
+int main(){
+
+	int input = 36000000; 
+
+	int house_num = 1;
+
+	//for(int i = 0; i < 10; i++){	
+	while(sum_presents(house_num) < input){
+		house_num++;
+	}
+	
+	cout << house_num << endl;
+
+    return 0;
+}
+
+int sum_presents(int n){
+	int sum = 0;
+	int d = (int)sqrt((double) n) + 1;
+	for(int i = 1; i <= d; i++){
+		if(n % i == 0){
+			sum += i;
+			sum += n/i;
+		}
+	}
+	return sum * 10;
+}
